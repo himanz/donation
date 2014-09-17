60fbbb2064d408565af06cfb83f0372b2f872a30
Jonathan Chang <jonoschang7@gmail.com>
2014-09-17 18:53:42 -0400

    describe tests for contributions controller

---
+++ spec/controllers/contributions_controller_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe ContributionsController do
+  describe 'GET #index' do
+  	context 'without params' do
+  		it "populates an array of all contributions"
+  		it "renders the :index view"
+  	end
+  end
+
+  describe 'GET#show' do
+  	it "assigns the requested contribution to @contribution"
+  	it "renders the :show template"
+  end
+
+  describe 'GET #new' do
+  	it "assigns a new Contribution to @contribution"
+    it "renders the :new template"
+  end
+
+  describe "POST #create" do
+  	context "with valid attributes" do
+  		it "saves the new contribution in the database"
+  		it "redirects to users#show"
+  	end
+
+  	context "with invalid attributes" do
+  		it "does not save the new contribution in the database"
+  		it "re-renders the :new template"
+  	end
+  end
+end
