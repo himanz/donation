7491d6614e3f5617ce7fa55b4682c018c86c015a
Jonathan Chang <jonoschang7@gmail.com>
2014-09-17 14:08:50 -0400

    create static pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -54,5 +54,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'home#index'
+  root 'static_pages#home'
 end
