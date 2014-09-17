138c458ca9bdf6a55f17697a820db356829b1fa7
Jonathan Chang <jonoschang7@gmail.com>
2014-09-17 16:01:50 -0400

    create get user#show test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,11 @@ require 'spec_helper'
 
 describe UsersController do
   describe "GET #show" do
-  	it "assigns the requested user to @user"
+  	it "assigns the requested user to @user" do
+  		user = create(:user)
+      get :show, id: user
+      expect(assigns(:user)).to eq contact
+  	end
   	it "renders the :show template"
   end
 end
