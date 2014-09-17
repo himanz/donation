f975218f533505c570e59d76db9cbdc7e80f8554
Jonathan Chang <jonoschang7@gmail.com>
2014-09-17 16:32:20 -0400

    test for rendering users show template

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,6 +7,11 @@ describe UsersController do
       get :show, id: user
       expect(assigns(:user)).to eq user
   	end
-  	it "renders the :show template"
+
+  	it "renders the :show template" do
+  		user = create(:user)
+  		get :show, id: user
+  		expect(response).to render_template :show
+  	end
   end
 end
