f0ecaacc19bc82880f7444539d700539c4fc7d6b
Jonathan Chang <jonoschang7@gmail.com>
2014-09-17 15:40:17 -0400

    test for static pages home route

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe StaticPagesController do
-
+  describe "home route" do
+  	it "routes to home" do
+  		get :home
+  		expect(:response).to render_template :home
+    end
+  end
 end
