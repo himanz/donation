8fd8ef466c64a7effa5e34233a7e3542a404e0c7
Jonathan Chang <jonoschang7@gmail.com>
2014-09-18 18:02:44 -0400

    refactor users controller get show tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,24 +2,23 @@ require 'spec_helper'
 
 describe UsersController do
   describe "GET #show" do
+  	before do
+  		@user = create(:user)
+  		get :show, id: @user
+  	end
+
   	it "assigns the requested user to @user" do
-  		user = create(:user)
-      get :show, id: user
-      expect(assigns(:user)).to eq user
+      expect(assigns(:user)).to eq @user
   	end
 
   	it "populates an array with all contributions for user" do
-  		user = create(:user)
-  		contribution1 = create(:contribution, user_id: user.id)
-  		contribution2 = create(:contribution, user_id: user.id)
-  		hello = "hello"
-  		get :show, id: user
+  		contribution1 = create(:contribution, user_id: @user.id)
+  		contribution2 = create(:contribution, user_id: @user.id)
+  		
   		expect(assigns(:contributions)).to match_array([contribution1, contribution2])
   	end
 
   	it "renders the :show template" do
-  		user = create(:user)
-  		get :show, id: user
   		expect(response).to render_template :show
   	end
   end
