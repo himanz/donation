bf14a854a13b0cec1ceb98fb77b88f763edfb76f
Jonathan Chang <jonoschang7@gmail.com>
2014-09-18 17:57:18 -0400

    grab all contributions of user for show user action

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -8,6 +8,15 @@ describe UsersController do
       expect(assigns(:user)).to eq user
   	end
 
+  	it "populates an array with all contributions for user" do
+  		user = create(:user)
+  		contribution1 = create(:contribution, user_id: user.id)
+  		contribution2 = create(:contribution, user_id: user.id)
+  		hello = "hello"
+  		get :show, id: user
+  		expect(assigns(:contributions)).to match_array([contribution1, contribution2])
+  	end
+
   	it "renders the :show template" do
   		user = create(:user)
   		get :show, id: user
